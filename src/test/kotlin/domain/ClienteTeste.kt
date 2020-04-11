d7a4a2007b87bf52e845359052bb04812a870db6
viniciuspessoni <Ch@conete7>
2020-04-11 12:22:48 +0100

    Adicionando teste de unidade para calculo de Risco do Cliente

---
+++ src/test/kotlin/domain/ClienteTeste.kt
@@ -0,0 +1,24 @@
+package domain
+
+import com.viniciuspessoni.domain.Cliente
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class ClienteTeste {
+
+    @Test
+    fun verficaCalculoDeRisco() {
+        val clienteEsperado = Cliente(
+            nome = "James Bond",
+            idade = 35,
+            id = 1007,
+            risco = -65
+        )
+        val clienteObtido = Cliente(
+            nome = "James Bond",
+            idade = 35,
+            id = 1007)
+
+        assertThat(clienteEsperado.risco).isEqualTo(clienteObtido.calcularRisco())
+    }
+}
