e2fda6f2da3c4be3eae3e072471b965fa003762a
viniciuspessoni <Ch@conete7>
2021-04-06 15:47:48 +0100

    WIP segurança

--- src/main/kotlin/com/viniciuspessoni/config/ConfiguracaoSeguraBasica.kt
+++ src/main/kotlin/com/viniciuspessoni/config/ConfiguracaoSeguraBasica.kt
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-class ConfiguracaoSegura : WebSecurityConfigurerAdapter() {
+class ConfiguracaoSeguraBasica : WebSecurityConfigurerAdapter() {
 
 
     @Autowired
---
+++ src/main/kotlin/com/viniciuspessoni/config/ConfiguracaoSeguraOauth.java
@@ -0,0 +1,12 @@
+package com.viniciuspessoni.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class ConfiguracaoSeguraOauth {
+
+
+
+}
